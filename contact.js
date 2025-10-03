f4746b98a4aedc7e52a2e9d9cdcf4cc29b5305b7
harpreetn01727992 <n01727992@humber.ca>
2025-10-03 19:14:56 -0400

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,13 @@
+import Navbar from "../components/Navbar";
+
+export default function Contact() {
+  return (
+    <div>
+      <Navbar />
+      <main style={{ padding: "2rem" }}>
+        <h1>Contact Page</h1>
+        <p>You can reach us at: example@email.com</p>
+      </main>
+    </div>
+  );
+}
